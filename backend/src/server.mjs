ec45cbcabdbff5b0b9b5b3286a06fa816faa08b5
h3kar360 <80110177+h3kar360@users.noreply.github.com>
2025-04-24 21:28:43 +0700

    detailing port

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -5,17 +5,17 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 
 mongoose
-  .connect("mongodb://127.0.0.1/todos_db")
-  .then(() => console.log("connected to database"))
-  .catch((err) => console.log(err));
+    .connect("mongodb://127.0.0.1/todos_db")
+    .then(() => console.log("connected to database"))
+    .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
-  console.log(`listening to port ${PORT}`);
+    console.log(`listening to port ${PORT}`);
 });
